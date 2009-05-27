3a8c601fd74b04e4a25d88a5cebc12b3c3f1a65f
Nicholas Fine <nick@isotope11.com>
2009-05-27 11:24:21 -0500

    admin product view works!

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ProductsController < Admin::BaseController
   unloadable # This is required if you subclass a controller provided by the base rails app
   layout 'admin'
-  before_filter :load_product,     :only => [:show, :edit, :update, :destroy]
+  before_filter :load_product,     :only => [:show, :edit, :update, :destroy, :create_quantity_discount]
   before_filter :load_new_product, :only => [:new, :create]
   before_filter :load_products,    :only => [:index]
 
@@ -18,63 +18,30 @@ class Admin::ProductsController < Admin::BaseController
     @products = Ansuz::NFine::Product.find(:all, :order => 'created_at DESC')
   end
 
-  # GET /products
-  # GET /products.xml
+  public
   def index
-    @products = Ansuz::NFine::Product.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @products }
-    end
   end
 
-  # GET /products/1
-  # GET /products/1.xml
   def show
-    @product = Ansuz::NFine::Product.find(params[:id])
-    @quantity_discounts = @product.quantity_discounts
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @product }
-    end
   end
 
-  # GET /products/new
-  # GET /products/new.xml
   def new
-    @product = Ansuz::NFine::Product.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @product }
-    end
   end
 
-  # GET /products/1/edit
   def edit
-    @product = Ansuz::NFine::Product.find(params[:id])
   end
 
-  # POST /products
-  # POST /products.xml
   def create
-    @product = Ansuz::NFine::Product.new(params[:product])
-
-    respond_to do |format|
       if @product.save
-        flash[:notice] = 'Ansuz::NFine::Product was successfully created.'
-        format.html { redirect_to(@product) }
+        flash[:notice] = 'Product was successfully created.'
+        redirect_to admin_products_path
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @product.errors, :status => :unprocessable_entity }
+        flash.now[:error] = "There was a problem creating the Product.  Please try again."
+        render :action => "new" 
       end
-    end
   end
 
   def create_quantity_discount
-    @product = Ansuz::NFine::Product.find(params[:id])
     @product.has_quantity_discount = true
     @product.save
     @quantity_discount = @product.quantity_discounts.create
@@ -89,29 +56,21 @@ class Admin::ProductsController < Admin::BaseController
   # PUT /products/1
   # PUT /products/1.xml
   def update
-    @product = Ansuz::NFine::Product.find(params[:id])
-
-    respond_to do |format|
       if @product.update_attributes(params[:product])
-        flash[:notice] = 'Ansuz::NFine::Product was successfully updated.'
-        format.html { redirect_to(@product) }
-        format.xml  { head :ok }
+        flash[:notice] = 'Product was successfully updated.'
+        redirect_to admin_product_path(@product)
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @product.errors, :status => :unprocessable_entity }
+        flash.now[:error] = "There was a problem updating the Product.  Please Try again."
+        render :action => 'edit'
       end
-    end
   end
 
   # DELETE /products/1
   # DELETE /products/1.xml
   def destroy
-    @product = Ansuz::NFine::Product.find(params[:id])
     @product.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(products_url) }
-      format.xml  { head :ok }
-    end
+    flash[:notice] = "Product was deleted."
+    redirect_to admin_products_path 
   end
+
 end
