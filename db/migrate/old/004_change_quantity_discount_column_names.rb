b1f88518dab1138854a960223946aae9341a5e26
Nicholas Fine <nicholas.fine@gmail.com>
2009-06-19 09:20:20 -0500

    removed extraneous migrations

--- db/migrate/old/004_change_quantity_discount_column_names.rb
+++
@@ -1,9 +0,0 @@
-class ChangeQuantityDiscountColumnNames < ActiveRecord::Migration
-  def self.up
-    rename_column :products, :quantity_discount, :has_quantity_discount
-  end
-
-  def self.down
-    rename_column :products, :has_quantity_discount, :quantity_discount
-  end
-end
--- db/migrate/old/008_add_description_to_product.rb
+++
@@ -1,9 +0,0 @@
-class AddDescriptionToProduct < ActiveRecord::Migration
-  def self.up
-    add_column :products, :description, :string
-  end
-
-  def self.down
-    remove_column :products, :description
-  end
-end
